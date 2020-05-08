a7f27a73b3892d64d0f4bfa276dcddf09b6b0cbf
Hou Ming Loke <s10195290@connect.np.edu.sg>
2020-05-09 04:30:10 +0800

    Completed

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,7 +2,10 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        VideoView rabbit = findViewById(R.id.videoView);
+        rabbit.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid));
+        rabbit.start();
+
+        MediaPlayer babyLaugh = MediaPlayer.create(MainActivity.this,R.raw.baby_laughing);
+        babyLaugh.start();
     }
 }
